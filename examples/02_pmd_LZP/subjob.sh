36accd34665075330e530b2c12ad5198265fc76c
Ryo KOBAYASHI <ryo.kbys@gmail.com>
2025-08-14 11:57:54 +0900

    modified according to the change of formats in pmd.

--- examples/02_pmd_LZP/subjob.sh
+++ examples/02_pmd_LZP/subjob.sh
@@ -25,9 +25,9 @@ head -n166 out.pmd.NpT
 tail -n20 out.pmd.NpT
 echo "NpT-MD done at" `date`
 #...extract rdf, adf, vol and rename files
-python ~/src/nap/nappy/rdf.py --fortran -d 0.05 -r 5.0 --gsmear=2 --skip=80 --specorder=Li,Zr,P,O --pairs=Li-O,Zr-O,P-O,Li-Li --out4fp -o data.pmd.rdf dump_* 2>&1 
-python ~/src/nap/nappy/adf.py --fortran --gsmear=2 --triplets=Zr-O-O,P-O-O --skip=80 --out4fp -o data.pmd.adf dump_* 2>&1 
-python ~/src/nap/nappy/vol_lat.py --skip=80 --out4fp --prefix data.pmd dump_* 2>&1
+python ~/src/nap/nappy/rdf.py --fortran -d 0.05 -r 5.0 --gsmear=2 --skip=80 --specorder=Li,Zr,P,O --pairs=Li-O,Zr-O,P-O,Li-Li --out4fp -o data.pmd.rdf traj.extxyz 2>&1
+python ~/src/nap/nappy/adf.py --fortran --gsmear=2 --triplets=Zr-O-O,P-O-O --skip=80 --out4fp -o data.pmd.adf traj.extxyz 2>&1
+python ~/src/nap/nappy/vol_lat.py --skip=80 --out4fp --prefix data.pmd traj.extxyz 2>&1
 t1=`date +%s`
 etime=`expr $t1 - $t0`
 echo "subjob.sh took" $etime "sec, done at" `date`
